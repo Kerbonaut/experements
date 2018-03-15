30cddbc2286c1c13e113d29aa7d5ded662e3045a
Kerbonaut <36743626+Kerbonaut@users.noreply.github.com>
2018-03-15 23:48:46 +0300

    Add files via upload

--- helper.py
+++ helper.py
@@ -1,13 +1,11 @@
 import random
 import math
 #print('0'*64, '1'*64, '2'*10, sep = '\n')
-'''for i in range(64):
-    print('')
-    for j in range(64):
-        print('0', end = ' ')
+
+#print('0/'*64, '3/'*10)
 for i in range(10):
+    for i in range(64):
+        print(round((random.random()-0.5) * 2, 1), end = ' ')
+        #print(1.0, end=' ')
     print('')
-    for j in range(64):
-        print('1', end = ' ')'''
-print('1 '*64)
-print(2.4**(-321))
+#print(2.4**(-321))
--- neuro.py
+++ neuro.py
@@ -1,3 +1,38 @@
+from PIL import Image
+import math
+import os
+
+def load_neuronet(fin, num):
+    config = fin.readline()
+    lairs = list(map(str, config.split()))
+    first_lair = list(map(int, lairs[0].split('/')))
+    net = neuronet([lair(len(first_lair), 0, [neuron(i, 0, first_lair[i], []) for i in range(len(first_lair))], 0)], num, config)
+    for j in range(1, len(lairs)):
+        lair_a = lairs[j]
+        lair_i = list(map(int, lair_a.split('/')))
+        net.lairs.append(lair(len(lair_i), j, [neuron(i, 0, lair_i[i], list(map(float, fin.readline().split()))) for i in range(len(lair_i))], num))
+    return net
+
+def save_neuronet(net, fout):
+    fout.write(net.config)
+    for i in net.lairs[1:]:
+        for j in i.neurons:
+            print(j.weights_in)
+            for l in j.weights_in:
+                fout.write(str(l) + ' ')
+            fout.write('\n')
+
+def activate(net, input_data):
+    temp = [input_data]
+    for i in net.lairs[1:]:
+        temp += [[]]
+        for j in range(len(i.neurons)):
+            temp[-1] += [0]
+            for l in range(len(temp[-2])):
+                temp[-1][-1] += temp[-2][l] * i.neurons[j].weights_in[l]
+            temp[-1][-1] = math.tanh(temp[-1][-1])
+    return temp[-1]
+
 class neuron:
     def __init__(self, num, lairnum, type, weights_in):
         self.num = num
@@ -13,46 +48,42 @@ class lair:
         self.netnum = netnum
 
 class neuronet:
-    def __init__(self, lairs, num):
+    def __init__(self, lairs, num, config):
         self.lairs = lairs
         self.num = num
-
-def activate(net, input_data):
-    temp = [input_data]
-    for i in net.lairs[1:]:
-        temp += [[]]
-        #print([i.neurons[j].num for j in range(len(i.neurons))])
-        #print(i.neurons[10].num)
-        for j in range(len(i.neurons)):
-            temp[-1] += [0]
-            for l in range(len(temp[-2])):
-                #print(type(temp[-1]))
-                #print([i.neurons[j].num for j in range(len(i.neurons))])
-                #print(i.neurons[10].weights_in)
-                #print(j)
-                #print(i.neurons[j].num)
-                temp[-1][-1] += temp[-2][l] * i.neurons[j].weights_in[l]
-    return temp[-1]
+        self.config = config
 
 chek_cmd = 1 #if 0 - exit, if 1 - continue, if 2 - training
-fin = open('save_oldv01.txt', 'r')
-fout = open('save_newv01.txt', 'w')
-net0 = neuronet([lair(64, 0, [neuron(i, 0, 0, []) for i in range(64)], 0),
-                 lair(64, 1, [neuron(i, 1, 1, list(map(int, fin.readline().split()))) for i in range(64)], 0),
-                 lair(10, 2, [neuron(i, 2, 3, list(map(int, fin.readline().split()))) for i in range(64)], 0)], 0)
+fin = open('save_oldv02.txt', 'r')
+fout = open('save_newv02.txt', 'w')
+net0 = load_neuronet(fin, 0)
 while chek_cmd != 0:
     chek_cmd = int(input())
     if chek_cmd == 1:
-        input_data = list(map(int, input().split()))
+        #input_data = list(map(int, input().split()))
+        im = Image.open("test.png")
+        inp = list(im.getdata())
+        input_data = [(i[0] + i[1] + i[2]) / 777 for i in inp]
         print(*activate(net0, input_data))
     elif chek_cmd == 2:
-        training_data = open('training_data.txt', 'r')
-for i in net0.lairs[1:]:
-    for j in i.neurons:
-        print(j.weights_in)
-        for l in j.weights_in:
-            #print(str(l) + ' ')
-            fout.write(str(l) + ' ')
-        fout.write('\n')
+        training_pics = os.listdir(path = "training_pics")
+        fin_anss = open('training_anss.txt', 'r')
+        training_anss = list(map(int, fin_anss.readline().split()))
+        for i in range(len(training_anss)):
+            im = Image.open('training_pics/' + training_pics[i])
+            inp = list(im.getdata())
+            input_data = [(i[0] + i[1] + i[2]) / 777 for i in inp]
+            ans = activate(net0, input_data)
+            target_ans = [-1.0] * 10
+            target_ans[training_anss[i]] = 1.0
+            #print(ans, target_ans)
+            ers = [target_ans[j] - ans[j] for j in range(len(target_ans))]
+            error = 0
+            for j in ers:
+                error += j ** 2
+            error /= 2
+            print(error) #to be continued...
+        fin_anss.close()
+save_neuronet(net0, fout)
 fin.close()
 fout.close()
